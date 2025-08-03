0f925d49ce300482ae4c6d9692f37d8cf41cbf7f
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-08-03 21:17:29 +0300

    Setting up body validation

---
+++ src/users/users.controller.ts
@@ -0,0 +1,9 @@
+import { Body, Controller , Post } from '@nestjs/common';
+import { createUserDto } from './dtos/create-user.dto';
+@Controller('auth')
+export class UsersController {
+@Post("/signup")
+createUser(@Body() body:createUserDto){
+console.log(body)
+}
+}
