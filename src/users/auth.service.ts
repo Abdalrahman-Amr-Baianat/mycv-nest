49c91b7703d019a33a3fa1ef84add4fdf1368ce7
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-08-07 11:44:11 +0300

    watch setting up sessions

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -14,7 +14,20 @@ export class AuthService {
     }
     const salt = randomBytes(8).toString('hex');
     const hash = (await scrypt(password, salt, 32)) as Buffer;
-    const result = salt + '.' + hash.toString('hex');//TODO complete user creation 
+    const result = salt + '.' + hash.toString('hex'); 
+    const user = await this.userService.create(email, result);
+    return user;
   }
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.userService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (hash.toString('hex') !== storedHash) {
+      throw new BadRequestException('Wrong password');
+    }
+    return user;
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,26 +8,43 @@ import {
   Query,
   Delete,
   NotFoundException,
+  Session,
 } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
 import { updateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
-
+import { AuthService } from './auth.service';
 @Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+  @Get('colors/:color')
+  setColor(@Param('color') color: string, @Session() session: any) {
+    session.color = color;
+  }
+  @Get('/colors')
+  getColor(@Session() session: any) {
+    return session.color;
+  }
+
   @Post('/signup')
   createUser(@Body() body: createUserDto) {
-    this.usersService.create(body.email, body.password);
+    this.authService.signup(body.email, body.password);
+  }
+  @Post('signin')
+  signin(@Body() body: createUserDto) {
+    return this.authService.signin(body.email, body.password);
   }
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException('User not found'); 
     }
     return user;
   }
