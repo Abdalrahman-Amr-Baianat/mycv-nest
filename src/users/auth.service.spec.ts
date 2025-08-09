e409e549c790caf5d4486ee343f7e438ba505ca3
Abdalrahman Amr <abdalrahman.amr@baianat.net>
2025-08-09 20:30:51 +0300

    2:32 testing setup

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,10 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+it('can create and instance of auth service ', async () => {
+  const module = await Test.createTestingModule({
+    providers: [AuthService],
+  }).compile();
+  const service = module.get(AuthenticatorAssertio)
+});
